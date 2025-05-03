e081b3c500a4901abd5703862ec9386805f747e3
Guilherme Rodrigues <guilhermer_l@hotmail.com>
2025-05-03 15:33:23 -0300

    Adicionado e configurado o movimento dos canos na tela

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
-from obj import Obj
+from obj import Obj, Pipe
 import pygame
+import random
 
 class Game:
     
@@ -7,10 +8,14 @@ class Game:
         
         self.all_sprites = pygame.sprite.Group()
         
+        self.speed = 3
+        
         self.bg = Obj("assets/sky.png", 0, 0, self.all_sprites)
         self.bg2 = Obj("assets/sky.png", 360, 0, self.all_sprites)
         self.ground = Obj("assets/ground.png", 0, 476, self.all_sprites)
         self.ground2 = Obj("assets/ground.png", 360, 476, self.all_sprites)
+        
+        self.ticks = 0
     
     def draw(self, window):
         self.all_sprites.draw(window)
@@ -19,6 +24,7 @@ class Game:
         self.all_sprites.update()
         self.move_bg()
         self.move_ground()
+        self.spawn_pipes()
         
     def move_bg(self):
         self.bg.rect[0] -= 1
@@ -30,10 +36,18 @@ class Game:
             self.bg2.rect[0] = 360
         
     def move_ground(self):
-        self.ground.rect[0] -= 2
-        self.ground2.rect[0] -= 2
+        self.ground.rect[0] -= self.speed
+        self.ground2.rect[0] -= self.speed
         
         if self.ground.rect[0] <= -360:
             self.ground.rect[0] = 360
         if self.ground2.rect[0] <= -360:
             self.ground2.rect[0] = 360
+    
+    def spawn_pipes(self):
+        self.ticks += 1
+        if self.ticks >= random.randrange(90, 150):
+            self.ticks = 0
+            pos = random.randrange(285, 420)
+            pipe = Pipe("assets/pipe1.png", 360, random.randrange(285, 420), self.all_sprites, speed = self.speed)
+            pipe2 = Pipe("assets/pipe2.png", 360, pipe.rect[1] - 490, self.all_sprites, speed = self.speed)
--- obj.py
+++ obj.py
@@ -3,11 +3,26 @@ import pygame
 
 class Obj(pygame.sprite.Sprite):
     
-    def __init__(self, img, x, y, *groups):
+    def __init__(self, img, x, y, *groups, speed = 1):
         super().__init__(*groups)
         
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect()
         self.rect[0] = x
         self.rect[1] = y
+        self.speed = speed
 
+
+class Pipe(Obj):
+    
+    def __init__(self, img, x, y, *groups, speed = 3):
+        super().__init__(img, x, y, *groups, speed = speed)
+        
+    def update(self):
+        self.move()
+    
+    def move(self):
+        self.rect[0] -= self.speed
+        
+        if self.rect[0] <= -100:
+            self.kill()
